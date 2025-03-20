038d4c9bb58b647acc25e52ffe0d8e64403ae458
Leo-JG1024 <fer03gelacio@gmail.com>
2025-03-20 01:15:03 -0600

    Integración de la funcionalidad de Audio

--- src/app/Components/encryption-form/encryption-form.component.ts
+++ src/app/Components/encryption-form/encryption-form.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-encryption-form',
-  imports: [],
   templateUrl: './encryption-form.component.html',
-  styleUrl: './encryption-form.component.css'
+  styleUrls: ['./encryption-form.component.css']
 })
 export class EncryptionFormComponent {
+  nombre: string = ''; 
+  isListening: boolean = false; 
 
+  private recognition: any = null;
+
+
+  constructor() {
+    if ('webkitSpeechRecognition' in window) {
+      this.recognition = new (window as any).webkitSpeechRecognition();
+      this.recognition.continuous = true; 
+      this.recognition.interimResults = true; 
+
+      this.recognition.onresult = (event: any) => {
+        const transcript = event.results[event.resultIndex][0].transcript;
+        this.nombre = transcript; 
+      };
+
+
+      this.recognition.onstart = () => {
+        this.isListening = true; 
+      };
+
+ 
+      this.recognition.onend = () => {
+        this.isListening = false; 
+      };
+    }
+  }
+
+  startListening(): void {
+    if (this.recognition) {
+      if (this.isListening) {
+        this.recognition.stop(); 
+      } else {
+        this.recognition.start(); 
+      }
+    }
+  }
 }
